3a8be92bb9d4eb5bf41eac7905be21e6c7a80299
Viswanath Kambhampati <viswanath16@gmail.com>
2025-05-11 15:46:15 +0200

    Updated changes

--- community-forum-backend/api/index.js
+++ community-forum-backend/api/index.js
@@ -1,5 +1,6 @@
 // api/index.js
 const app = require('../dist/server').default;
 
-// Export the app for Vercel
-module.exports = app;
+module.exports = (req, res) => {
+    return app(req, res);
+};
